3efc40e2a90055f32eed6487d4f3a7a40f9fb326
Ilya Golubkov <ilya.golubkov@gmail.com>
2020-04-17 15:08:26 +0600

    Task popup markup

--- src/Components/UI/TaskPopupMenu/TaskPopupMenu.js
+++ src/Components/UI/TaskPopupMenu/TaskPopupMenu.js
@@ -1,15 +1,27 @@
 import React, { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { AppContext } from '../../../contexts/appContext';
+import { TasksContext } from '../../../contexts/taskContext';
 import * as actiontypes from '../../../contexts/actionTypes';
 
 import classes from './TaskPopupMenu.module.css';
 
 const TaskPopupMenu = (props) => {
   const { appState, appDispatch } = useContext(AppContext);
+  const { tasks } = useContext(TasksContext);
+  const { t } = useTranslation();
+
+  let selectedTask = null;
+
+  for (let task of tasks) {
+    if (task.id === props.id) {
+      selectedTask = { ...task };
+    }
+  }
 
   const mouseEnterHandler = () => {
-    if (appState.TaskPopupMenu.taskId !== null) {
+    if (appState.TaskPopupMenu.id !== null) {
       appDispatch({
         type: actiontypes.LOCK_TASK_POPUP_MENU,
       });
@@ -29,7 +41,10 @@ const TaskPopupMenu = (props) => {
       onMouseLeave={mouseLeaveHandler}
       style={{ top: props.y, left: props.x }}
     >
-      PopUp Menu
+      <div className={classes.header}>{selectedTask.task}</div>
+      <div className={classes.submenu}>{t('Edit')}</div>
+      <div className={classes.submenu}>{t('Done')}</div>
+      <div className={classes.submenu}>{t('Delete')}</div>
     </div>
   );
 };
