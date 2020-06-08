93beed7b14205b7d7da55badac312d153f9e25ee
Ilya Golubkov <ilya.golubkov@gmail.com>
2020-06-08 13:06:01 +0600

    Changed baseURL

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-//baseURL: "http://localhost:3030/"
-//baseURL: "http://45.143.137.231/"
+//baseURL: 'http://localhost:3030/'
+//baseURL: 'http://45.143.137.231/'
 const instance = axios.create({
-    baseURL: "http://45.143.137.231/"
-})
+  baseURL: 'http://45.143.137.231/',
+});
 
 export default instance;
